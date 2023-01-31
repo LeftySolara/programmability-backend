2eaabd47d7ff5fc6edf1118c2c2f658ba0cb400d
Julianne Adams <julianne@julianneadams.info>
2023-01-30 21:41:15 -0500

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
+  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
+  coverageReporters: ["html", "text", "text-summary", "cobertura"],
+  testMatch: ["**/*.test.ts"],
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "@components(.*)$": "<rootDir>/src/components$1",
+    "@loaders(.*)": "<rootDir>/src/loaders$1",
+    "@utils(.*)$": "<rootDir>/src/utils$1",
+  },
+};
